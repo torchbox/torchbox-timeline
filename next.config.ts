cf46a53cdd9d07b86dd2933348b1653eaeb5ca9c
Steven Steinwand <steven_ts@hotmail.com>
2025-12-10 18:51:16 -0700

    Update base path and image path

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,15 @@
 import type {NextConfig} from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
     output: 'export', // key part for static export
     images: {
         unoptimized: true, // needed for GitHub Pages
     },
     distDir: 'dist',
+    basePath: isProd ? '/torchbox-timeline' : '',
+    assetPrefix: isProd ? '/torchbox-timeline' : '',
 };
 
 export default nextConfig;
